060d9ec3e54599d59b1839f0c57f11716609a474
Certo <cervi01@ca.com>
2016-07-07 22:03:00 +0200

    Added Files

---
+++ resteasy/src/main/java/org/lab/resteasy/services/MyRestEasyApplication.java
@@ -0,0 +1,26 @@
+package org.lab.resteasy.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/services")
+public class MyRestEasyApplication  extends Application{
+	
+	private Set<Object> services;
+	
+	public MyRestEasyApplication()
+	{
+		services = new HashSet<Object>();
+		services.add(new PersonService());
+	}
+	
+	@Override
+	public Set<Object> getSingletons() {
+		// TODO Auto-generated method stub
+		return services;
+	}
+
+}
